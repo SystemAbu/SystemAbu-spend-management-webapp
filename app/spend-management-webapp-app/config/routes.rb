264177a8b0b6a9bac6d635813e9771488375e404
SystemAbu <abu.system.316@gmail.com>
2024-04-06 23:39:02 +0900

    救済措置

--- app/spend-management-webapp-app/config/routes.rb
+++ app/spend-management-webapp-app/config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :tests, only: [:index, :new, :create]
-
   namespace :api do
     namespace :v1 do
-      resources :test, only: [:index]
+      resources :groups
     end
   end
 end
--- app/spend-management-webapp-app/db/migrate/20231013125608_create_tests.rb
+++
@@ -1,8 +0,0 @@
-class CreateTests < ActiveRecord::Migration[7.0]
-  def change
-    create_table :tests do |t|
-      t.integer :pass
-      t.timestamps
-    end
-  end
-end
