33f5a7fe12b74f0ea134f8f7b91e747e4897596e
SystemAbu <abu.system.316@gmail.com>
2025-04-06 21:00:39 +0900

    環境構築
    * apiをヘッダーで制御し、URL直打ちを抑止

--- app/spend-management-webapp-app/config/application.rb
+++ app/spend-management-webapp-app/config/application.rb
@@ -45,5 +45,8 @@ module SpendManagementWebappApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # ミドルウェアに API キー認証を追加
+    require_relative '../app/middleware/api_key_auth_middleware'
+    config.middleware.use ApiKeyAuthMiddleware
   end
 end
